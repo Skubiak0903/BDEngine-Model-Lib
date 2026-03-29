f3eff74972a799e41facd15d551069426edbf2f6
Skubiak0903 <98041421+Skubiak0903@users.noreply.github.com>
2026-03-29 22:23:47 +0200

    Hopefully last change to get jitpack working

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.github.Skubiak0903"
-version = "0.1.0" 
+version = "0.1.1" 
 
 dependencies {
     implementation("net.minestom:minestom:2026.03.03-1.21.11")
@@ -33,5 +33,13 @@ tasks.test {
     testLogging {
         events("passed", "skipped", "failed")
         showStandardStreams = true
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
     }
 }
